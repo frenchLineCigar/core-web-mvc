58b38cdd29eef1c25447bff5d94ab8483d8ccdd3
frenchline <frenchline707@gmail.com>
2020-11-23 08:31:31 +0900

    Initial commit

---
+++ src/main/java/me/frenchline/corewebmvc/CoreWebMvcApplication.java
@@ -0,0 +1,13 @@
+package me.frenchline.corewebmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreWebMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreWebMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/frenchline/corewebmvc/CoreWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package me.frenchline.corewebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreWebMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
