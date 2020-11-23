3a8fa083683a986cebe01b1b32db9c3b68fce4f2
frenchline <frenchline707@gmail.com>
2020-11-23 22:05:40 +0900

    스프링 MVC 활용> 핸들러 메소드 : URI 패턴
    @PathVariable 복수개 받는 URI 설계 스타일
    
    @PathVariable
    ● 요청 URI 패턴의 일부를 핸들러 메소드 아규먼트로 받는 방법.
    ● 타입 변환 지원.
    ● (기본)값이 반드시 있어야 한다.
    ● Optional 지원.
    
    참고:
    https://docs.spring.io/spring/docs/current/spring-framework-reference/web.html#mvc-ann-typeconversion
    https://docs.spring.io/spring/docs/current/spring-framework-reference/web.html#mvc-ann-matrix-variables

--- src/main/java/me/frenchline/corewebmvc/SampleController.java
+++ src/main/java/me/frenchline/corewebmvc/SampleController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Optional;
+
 /**
  * @author swlee
  * @contact frenchline707@gmail.com
@@ -13,11 +15,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class SampleController {
 
-    @GetMapping("/events/{id}")
+    @GetMapping("/events/id/{id}/name/{name}")
     @ResponseBody
-    public Event getEvent(@PathVariable Integer id) { //String -> Integer 타입 변환
+    public Event getEvent(@PathVariable Integer id, @PathVariable String name) {
         Event event = new Event();
         event.setId(id);
+        event.setName(name);
         return event;
     }
 }
