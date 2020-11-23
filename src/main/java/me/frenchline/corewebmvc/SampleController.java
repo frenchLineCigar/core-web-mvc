c0fbb04e2913f5511d4ba66bf8227731421fa870
frenchline <frenchline707@gmail.com>
2020-11-23 19:12:31 +0900

    스프링 MVC 활용> HTTP 요청 맵핑 : 맵핑 연습
    다음 요청을 처리할 수 있는 핸들러 메소드를 맵핑하는 @RequestMapping (또는 @GetMapping,
    @PostMapping 등)을 정의하세요.
    
    1. GET /events
    
    2. GET /events/1, GET /events/2, GET /events/3, ...
    
    3. POST /events CONTENT-TYPE: application/json ACCEPT: application/json
    
    4. DELETE /events/1, DELETE /events/2, DELETE /events/3, ...
    
    5. PUT /events/1 CONTENT-TYPE: application/json ACCEPT: application/json, PUT /events/2 CONTENT-TYPE: application/json ACCEPT: application/json, ...

--- src/main/java/me/frenchline/corewebmvc/SampleController.java
+++ src/main/java/me/frenchline/corewebmvc/SampleController.java
@@ -1,8 +1,13 @@
 package me.frenchline.corewebmvc;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -12,12 +17,42 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @since 2019-11-23
  */
 @Controller
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE) //조회 시 모든 응답은 JSON 타입
 public class SampleController {
+
+
+    /**
+     * 1. GET /events
+     */
+    @GetMapping("/events")
+    @ResponseBody
+    public String getEvents() {
+        return "events";
+    }
+
+    /**
+     * 2. GET /events/1, GET /events/2, GET /events/3, ...
+     */
+    @GetMapping("/events/{id}")
+    @ResponseBody
+    public String getAnEvent(@PathVariable Long id) {
+        return "event";
+    }
+
+    /**
+     * 4. DELETE /events/1, DELETE /events/2, DELETE /events/3, ...
+     */
+    @DeleteMapping("/events/{id}")
+    @ResponseBody
+    public String removeAnEvent(@PathVariable Long id) {
+        return "event";
+    }
 
     @GetHelloMapping
     @ResponseBody
     public String hello() {
         return "hello";
     }
+
 
 }
--- src/test/java/me/frenchline/corewebmvc/SampleControllerTest.java
+++ src/test/java/me/frenchline/corewebmvc/SampleControllerTest.java
@@ -4,16 +4,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.hasItems;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -35,4 +35,78 @@ public class SampleControllerTest {
                 .andExpect(status().isOk())
         ;
     }
+
+    /**
+     * 1. GET /events
+     */
+    @Test
+    public void getEvents() throws Exception {
+        mockMvc.perform(get("/events"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    /**
+     * 2. GET /events/1, GET /events/2, GET /events/3, ...
+     */
+    @Test
+    public void getEventsWithId() throws Exception {
+        mockMvc.perform(get("/events/1"))
+                .andDo(print())
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/events/2"))
+                .andDo(print())
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/events/3"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    /**
+     * 3. POST /events CONTENT-TYPE: application/json ACCEPT: application/json
+     */
+    @Test
+    public void createEvent() throws Exception {
+        mockMvc.perform(
+                post("/events")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+        )
+                .andDo(print())
+                .andExpect(status().isOk())
+        ;
+    }
+
+    /**
+     * 4. DELETE /events/1, DELETE /events/2, DELETE /events/3, ...
+     */
+    @Test
+    public void deleteEvent() throws Exception {
+        mockMvc.perform(delete("/events/1"))
+                .andDo(print())
+                .andExpect(status().isOk());
+        mockMvc.perform(delete("/events/2"))
+                .andDo(print())
+                .andExpect(status().isOk());
+        mockMvc.perform(delete("/events/3"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    /**
+     * 5. PUT /events/1 CONTENT-TYPE: application/json ACCEPT: application/json,
+     * PUT /events/2 CONTENT-TYPE: application/json ACCEPT: application/json, ...
+     */
+    @Test
+    public void updateEvent() throws Exception {
+        mockMvc.perform(
+                put("/events")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+        )
+                .andDo(print())
+                .andExpect(status().isOk())
+        ;
+    }
+
 }
