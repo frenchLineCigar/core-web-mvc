250ec16e2d0587e52e4b7753eef70f6297b8bbf9
frenchline <frenchline707@gmail.com>
2020-11-23 21:37:40 +0900

    스프링 MVC 활용> 핸들러 메소드 : URI 패턴
    타입 변환(Type Conversion) 예
    String -> Integer
    
    @PathVariable
    ● 요청 URI 패턴의 일부를 핸들러 메소드 아규먼트로 받는 방법.
    ● 타입 변환 지원.
    ● (기본)값이 반드시 있어야 한다.
    ● Optional 지원.
    
    참고:
    https://docs.spring.io/spring/docs/current/spring-framework-reference/web.html#mvc-ann-typeconversion
    https://docs.spring.io/spring/docs/current/spring-framework-reference/web.html#mvc-ann-matrix-variables

---
+++ src/main/java/me/frenchline/corewebmvc/Event.java
@@ -0,0 +1,29 @@
+package me.frenchline.corewebmvc;
+
+/**
+ * @author swlee
+ * @contact frenchline707@gmail.com
+ * @since 2019-11-23
+ */
+public class Event {
+
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/test/java/me/frenchline/corewebmvc/SampleControllerTest.java
+++ src/test/java/me/frenchline/corewebmvc/SampleControllerTest.java
@@ -4,16 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -29,83 +25,11 @@ public class SampleControllerTest {
     MockMvc mockMvc;
 
     @Test
-    public void helloTest() throws Exception {
-        mockMvc.perform(get("/hello"))
-                .andDo(print())
-                .andExpect(status().isOk())
-        ;
-    }
-
-    /**
-     * 1. GET /events
-     */
-    @Test
-    public void getEvents() throws Exception {
-        mockMvc.perform(get("/events"))
-                .andDo(print())
-                .andExpect(status().isOk());
-    }
-
-    /**
-     * 2. GET /events/1, GET /events/2, GET /events/3, ...
-     */
-    @Test
-    public void getEventsWithId() throws Exception {
+    public void getEvent() throws Exception {
         mockMvc.perform(get("/events/1"))
                 .andDo(print())
-                .andExpect(status().isOk());
-        mockMvc.perform(get("/events/2"))
-                .andDo(print())
-                .andExpect(status().isOk());
-        mockMvc.perform(get("/events/3"))
-                .andDo(print())
-                .andExpect(status().isOk());
-    }
-
-    /**
-     * 3. POST /events CONTENT-TYPE: application/json ACCEPT: application/json
-     */
-    @Test
-    public void createEvent() throws Exception {
-        mockMvc.perform(
-                post("/events")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON)
-        )
-                .andDo(print())
                 .andExpect(status().isOk())
-        ;
-    }
-
-    /**
-     * 4. DELETE /events/1, DELETE /events/2, DELETE /events/3, ...
-     */
-    @Test
-    public void deleteEvent() throws Exception {
-        mockMvc.perform(delete("/events/1"))
-                .andDo(print())
-                .andExpect(status().isOk());
-        mockMvc.perform(delete("/events/2"))
-                .andDo(print())
-                .andExpect(status().isOk());
-        mockMvc.perform(delete("/events/3"))
-                .andDo(print())
-                .andExpect(status().isOk());
-    }
-
-    /**
-     * 5. PUT /events/1 CONTENT-TYPE: application/json ACCEPT: application/json,
-     * PUT /events/2 CONTENT-TYPE: application/json ACCEPT: application/json, ...
-     */
-    @Test
-    public void updateEvent() throws Exception {
-        mockMvc.perform(
-                put("/events")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON)
-        )
-                .andDo(print())
-                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
         ;
     }
 
