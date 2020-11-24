95e4cfcb7ce860addc18d8f1788dc14c2b350c4d
frenchline <frenchline707@gmail.com>
2020-11-24 12:01:52 +0900

    스프링 MVC 활용> 핸들러 메소드 : @SessionAttributes, @ModelAttribute, SessionStatus
    모델 정보를 HTTP 세션에 저장해주는 애노테이션
    ● HttpSession을 직접 사용할 수도 있지만
    ● 이 애노테이션에 설정한 이름에 해당하는 모델 정보를 자동으로 세션에 넣어준다.
    ● @ModelAttribute는 세션에 있는 데이터도 바인딩한다.
    ● 여러 화면(또는 요청)에서 사용해야 하는 객체를 공유할 때 사용한다.
    
    SessionStatus를 사용해서 세션 처리 완료를 알려줄 수 있다.
    ● 폼 처리 끝나고 세션을 비울 때 사용한다.
    
    왜 모델 애트리뷰트를 세션에도 넣는가?
    가령 장바구니 데이터가 있다.
    여러 페이지에 걸쳐서 장바구니에 있는 데이터는 계속해서 동일하게 유지되야 한다.
    또는 어떤 데이터를 생성할 때 여러 화면에 걸쳐서 만들어야 되는 경우가 있다.
    입력받는 폼의 값이 너무 많은 경우에는 화면을 나눌 수도 있다.
    첫번째 폼입력 화면에서는 name만 받고, name이 제대로 입력됐으면
    그다음 폼에서 limit이란 값을 받고, limit까지 값이 완료됐으면
    이제 첫번째 폼에서 받은 name과 두번째 폼에서 받은 limit을 조합해서 event 객체를 저장하면 된다.
    이때 limit라는 값을 입력하는 폼으로 가기 전에 name이 입력된 event 객체를 세션에 저장해두고 재사용하는 것이다.
    어떻게 그럴 수 있는가?
    @ModelAttribute가 세션에 있는 값도 조합을 해서 복합(Composed) 객체에 넣어주기 때문이다.

--- src/main/java/me/frenchline/corewebmvc/SampleController.java
+++ src/main/java/me/frenchline/corewebmvc/SampleController.java
@@ -7,11 +7,10 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,25 +20,29 @@ import java.util.List;
  * @since 2019-11-23
  */
 @Controller
+@SessionAttributes("event") //모델의 애트리뷰트 중 event를 세션에도 자동으로 넣어줌
 public class SampleController {
 
     @GetMapping("/events/form")
-    public String eventsForm(Model model, HttpSession httpSession) {
+    public String eventsForm(Model model) {
         Event newEvent = new Event();
         newEvent.setLimit(50);
-        model.addAttribute("event", newEvent);
-        httpSession.setAttribute("event", newEvent);
+        model.addAttribute("event", newEvent); //세션에도 같이 저장됨
         return "/events/form";
     }
 
     @PostMapping("/events")
     public String createEvent(@Validated @ModelAttribute Event event,
-                           BindingResult bindingResult) {
+                              BindingResult bindingResult,
+                              SessionStatus sessionStatus) {
         if (bindingResult.hasErrors()) {
             return "/events/form";
         }
 
         //save
+
+        //특정한 폼 처리가 끝났을 때 세션이 만료되도록 처리
+        sessionStatus.setComplete();
 
         return "redirect:/events/list";
     }
--- src/test/java/me/frenchline/corewebmvc/SampleControllerTest.java
+++ src/test/java/me/frenchline/corewebmvc/SampleControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -35,12 +36,14 @@ public class SampleControllerTest {
 
     @Test
     public void eventForm() throws Exception {
-        mockMvc.perform(get("/events/form"))
+        MockHttpServletRequest request = mockMvc.perform(get("/events/form"))
                 .andDo(print())
                 .andExpect(view().name("/events/form"))
                 .andExpect(model().attributeExists("event"))
                 .andExpect(request().sessionAttribute("event", notNullValue()))
-        ;
+                .andReturn().getRequest();
+        Object event = request.getSession().getAttribute("event"); //@SessionAttributes("event") 작동 확인
+        System.out.println("event = " + event);
     }
 
     @Test
