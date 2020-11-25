c142a4ebf93ed7558f8612c7df35409108915834
frenchline <frenchline707@gmail.com>
2020-11-26 02:38:37 +0900

    스프링 MVC 활용> 데이터 바인더(DataBinder) : @InitBinder
    Data를 바인딩할때 사용되는 DataBinder는 커스터마이징이 가능하다.
    
    @InitBinder
    ● Spring 2.5 부터 지원
    ● @InitBinder(키)
    - 특정 컨트롤러에서 바인딩 혹은 검증 설정을 커스터마이징할때 사용한다.
    ● 리턴값은 반드시 void 여야한다.
    ● Method LEVEL에 선언한다.
    ● WebDataBinder 객체를 Argument로 받아 커스터마이징을 할수 있다.
    
    특정 컨트롤러에서 바인딩 또는 검증 설정을 변경하고 싶을 때 사용
    ```
    @InitBinder
    public void initEventBinder(WebDataBinder webDataBinder) {
        webDataBinder.setDisallowedFields("id");
    }
    ```
    
    바인딩 설정
    ● webDataBinder. setDisallowedFields("필드명"); 바인딩을 받고싶지 않은 필드를 설정
    
    참고
    ● https://docs.spring.io/spring/docs/current/spring-framework-reference/web.html#mvc-ann-initbinder
    ● https://github.com/spring-projects/spring-petclinic/blob/master/src/main/java/org/springframework/samples/petclinic/owner/PetController.java

--- src/main/java/me/frenchline/corewebmvc/EventController.java
+++ src/main/java/me/frenchline/corewebmvc/EventController.java
@@ -1,12 +1,12 @@
 package me.frenchline.corewebmvc;
 
-import org.springframework.context.i18n.LocaleContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
@@ -19,7 +19,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-import static java.util.Locale.*;
+import static java.util.Locale.FRANCE;
+import static java.util.Locale.ITALY;
+import static java.util.Locale.KOREA;
+import static java.util.Locale.UK;
+import static java.util.Locale.US;
 
 /**
  * @author swlee
@@ -29,6 +33,23 @@ import static java.util.Locale.*;
 @Controller
 @SessionAttributes("event")
 public class EventController {
+
+    /**
+     * DataBinder : @InitBinder
+     * 데이터들을 바인딩할 때 사용하는 데이터 바인드를 커스터마이징 할 수 있다
+     * 특정 컨트롤러 안에서 사용하는 데이터 바인드 설정 : @InitBinder 애노테이션을 붙인 메서드를 정의해서 바인딩 설정을 변경할 수 있다
+     * - 리턴 타입은 반드시 void 이어야 하고, 메서드명 제약은 없다.
+     * - WebDataBinder 아규먼트는 반드시 있어야 한다
+     * setDisallowedFields : 받고 싶지 않은 필드값을 걸러낼 수 있다. (블랙 리스트)
+     * setAllowedFields : 받고 싶은 필드값을 명기한다. (화이트 리스트)
+     * - id는 이벤트를 저장할 때 생성하고 싶기 때문에 id값을 폼이나 쿼리 패스나 쿼리 파라미터 등으로 받아오고 싶지 않다면
+     * - id를 폼에서 보내더라도 걸러내고 바인딩 하지 않는다
+     */
+    /* 모든 요청 전에 호출 */
+    @InitBinder
+    public void initEventBinder(WebDataBinder webDataBinder) {
+        webDataBinder.setDisallowedFields("id");
+    }
 
     @ModelAttribute
     public void categories(Model model) {
